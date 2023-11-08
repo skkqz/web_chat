8a915312c94b65b2cfbc79e72c9ca20d2f22c821
Andrew <skkqw@yandex.ru>
2023-11-08 22:29:16 +0300

    Добавлена модель сообщения

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from accounts.models import ProfileUserModel
+
+User = get_user_model()
+
+
+class MessageChatModel(models.Model):
+    """
+    Модель сообщения чата
+    """
+
+    sender = models.ForeignKey(ProfileUserModel, on_delete=models.CASCADE)
+    message = models.TextField(blank=True, null=True)
+    thread_name = models.CharField(max_length=100, blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.message
+
+
+# class ChatNotification(models.Model):
+#     """
+#     Модель уведомлений в чате
+#     """
+#     chat = models.ForeignKey(MessageChatModel, on_delete=models.CASCADE)
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     is_seen = models.BooleanField(default=False)
+#
+#     def __str__(self) -> str:
+#         return self.user.username
+#
--- chat/views.py
+++ chat/views.py
@@ -4,6 +4,8 @@ from django.views.generic import TemplateView
 
 from django.contrib.auth import get_user_model
 
+from .models import MessageChatModel
+
 User = get_user_model()
 
 
@@ -24,7 +26,13 @@ class ChatView(LoginRequiredMixin, TemplateView):
     def get_context_data(self, **kwargs):
         user = User.objects.get(username=self.kwargs['username'])
 
+        if self.request.user.id > user.id:
+            thread_name = f'chat_{self.request.user.id}-{user.id}'
+        else:
+            thread_name = f'chat_{user.id}-{self.request.user.id}'
+
         context = super().get_context_data(**kwargs)
         context['users'] = User.objects.exclude(username=self.request.user.username)
         context['user'] = user
+        context['messages'] = MessageChatModel.objects.filter(thread_name=thread_name)
         return context
