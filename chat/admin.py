8bd2683f7cebe026b9d0f62be7b959124c81384f
Andrew <skkqw@yandex.ru>
2023-11-08 22:30:20 +0300

    +

--- chat/admin.py
+++ chat/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from django.contrib.auth.models import User
+from .models import MessageChatModel
 
 
+@admin.register(MessageChatModel)
+class MessageChatAdmin(admin.ModelAdmin):
+
+    pass
