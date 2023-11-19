d5b8377074ca4504b034eb9e6c765e7f75180329
Andrew <skkqw@yandex.ru>
2023-11-19 21:17:44 +0300

    Исправлено создание и отображение комнат чата

--- chat/services.py
+++ chat/services.py
@@ -1,3 +1,13 @@
+from django.db.models import Q
+from django.db.models.query import QuerySet
+from django.contrib.auth import get_user_model
+
+from .models import MessageChatModel, RoomChatModel
+from accounts.models import ProfileUserModel
+
+User = get_user_model()
+
+
 def create_chat_name(user_1: int, user_2: int) -> str:
     """
     Создание имени комнаты чата
@@ -12,3 +22,50 @@ def create_chat_name(user_1: int, user_2: int) -> str:
         room_name = f'chat_{user_2}-{user_1}'
 
     return room_name
+
+
+def get_chat_room(room_name: str, user_1, user_2) -> QuerySet:
+    """
+    Получение или создание комнаты чата
+    :param room_name: Имя комнаты
+    :param user_1: Пользователь 1
+    :param user_2: Пользователь 2
+    :return: RoomChatModel objects
+    """
+
+    try:
+        room = RoomChatModel.objects.get(name=room_name)
+    except RoomChatModel.DoesNotExist:
+        room = RoomChatModel.objects.create(
+            name=room_name,
+            user_first=ProfileUserModel.objects.get(user=user_1),
+            user_second=ProfileUserModel.objects.get(user=user_2)
+        )
+
+    return room
+
+
+def get_list_rooms_users(user: User) -> QuerySet:
+    """
+    Получение списка пользователей с которыми есть комната чата
+    :param user: User objects
+    :return: QuerySet список пользователей
+    """
+
+    # Получаем все комнаты, в которых участвует текущий пользователь
+    user_rooms = RoomChatModel.objects.filter(
+        Q(user_first=ProfileUserModel.objects.get(user=user)) |
+        Q(user_second=ProfileUserModel.objects.get(user=user))
+    )
+
+    # Получаем список пользователей с которыми есть чат
+    users_with_chat = ProfileUserModel.objects.filter(
+        Q(user_1__in=user_rooms) | Q(user_2__in=user_rooms)
+    ).exclude(user=user)
+
+    # Получаем список имен пользователей
+    users_with_chat_names = users_with_chat.values_list('user', flat=True)
+
+    print(users_with_chat)
+
+    return users_with_chat
--- chat/views.py
+++ chat/views.py
@@ -1,11 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from django.db.models import Q
 
 from django.contrib.auth import get_user_model
 
-from .services import create_chat_name
+from .services import create_chat_name, get_list_rooms_users, get_chat_room
 from .models import MessageChatModel, RoomChatModel
 from accounts.models import ProfileUserModel
 
@@ -17,7 +16,7 @@ class IndexView(LoginRequiredMixin, TemplateView):
     Представление домашней страницы
     """
 
-    template_name = 'chat/chat_main.html'
+    template_name = 'chat/index.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -30,7 +29,7 @@ class ChatView(LoginRequiredMixin, TemplateView):
     Представление чата
     """
 
-    template_name = 'chat/index.html'
+    template_name = 'chat/chat_main.html'
 
     def get_context_data(self, **kwargs):
         user = User.objects.get(username=self.kwargs['username'])
@@ -40,27 +39,16 @@ class ChatView(LoginRequiredMixin, TemplateView):
             user_2=user.id,
         )
 
-        room = RoomChatModel.objects.get_or_create(name=room_name,
-                                                   user_first=ProfileUserModel.objects.get(user=self.request.user),
-                                                   user_second=ProfileUserModel.objects.get(user=user))
-
-        # Получаем все комнаты, в которых участвует текущий пользователь
-        user_rooms = RoomChatModel.objects.filter(
-            Q(user_first=ProfileUserModel.objects.get(user=self.request.user)) |
-            Q(user_second=ProfileUserModel.objects.get(user=self.request.user))
+        room = get_chat_room(
+            room_name=room_name,
+            user_1=self.request.user,
+            user_2=user
         )
 
-        # Получаем список пользователей с которыми есть чат
-        users_with_chat = ProfileUserModel.objects.filter(
-            Q(user_1__in=user_rooms) | Q(user_2__in=user_rooms)
-        ).exclude(user=self.request.user)
-
-        # Получаем список имен пользователей
-        users_with_chat_names = users_with_chat.values_list('user__username', flat=True)
-        print(users_with_chat_names)
+        users_with_chat_names = get_list_rooms_users(self.request.user)
 
         context = super().get_context_data(**kwargs)
-        context['users'] = User.objects.exclude(username=self.request.user.username)
+        context['users'] = users_with_chat_names
         context['user'] = user
-        context['messages'] = MessageChatModel.objects.filter(room_name=room[0])
+        context['messages'] = MessageChatModel.objects.filter(room_name=room)
         return context
